61f1c74c88d08549905a918c3bb4eb4ef56af39d
bahardns <bahardns@hotmail.com>
2023-03-08 01:13:45 +0300

    variables using

--- src/intro/Main.java
+++ src/intro/Main.java
@@ -3,7 +3,18 @@ package intro;
 public class Main {
 
 	public static void main(String[] args) {
+		//snippet: hazır kod
 		System.out.println("hello world");
+		
+		String metin="bahar";
+		System.out.println(metin);
+		
+		String Ortametin="danis";
+		System.out.println(Ortametin);
+		//javada kod satırları ; ile bitirilmeli
+		//değişken isimlendirmeleri javada camselCase(baharDns) yazılır.
+		
+		
 
 	}
 
